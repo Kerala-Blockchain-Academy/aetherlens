ebca488c694cf481b80b8133bd8c92adcfd5fcae
pglekshmi <pglekshmi88@gmail.com>
2025-02-28 15:46:30 +0530

    🗄️| DB added

--- api/db/db.go
+++ api/db/db.go
@@ -2,10 +2,10 @@ package db
 
 import (
 	"fmt"
-	"log"
-	"time"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
 func Connect() (*gorm.DB, error) {
@@ -14,25 +14,23 @@ func Connect() (*gorm.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to DB: %w", err)
 	}
-	fmt.Println("error",err)
-	 err = db.Raw("SELECT 1").Error
-	 fmt.Println("error2",err)
+
+	err = db.Raw("SELECT 1").Error
+
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to DB: %w", err)
 	}
 
-	
 	return db, nil
 }
 
 func RetryConnectDB(maxRetries int, delay time.Duration) (*gorm.DB, error) {
-	var db *gorm.DB 
+	var db *gorm.DB
 	var err error
 
 	for i := 1; i <= maxRetries; i++ {
 		db, err = Connect()
-		fmt.Println("db",db)
-		
+
 		if db != nil {
 			fmt.Println("Successfully connected to node!")
 			return db, nil
