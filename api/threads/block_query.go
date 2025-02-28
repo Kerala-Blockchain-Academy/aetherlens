e478c639ddddaac94085349ce39d14efd919d4d0
pglekshmi <pglekshmi88@gmail.com>
2025-02-28 15:48:26 +0530

    🚀| go thread

--- api/threads/block_query.go
+++ api/threads/block_query.go
@@ -3,16 +3,16 @@ package threads
 import (
 	"context"
 	"fmt"
-	"log"
-	"time"
 	"github.com/pglekshmi/explorerGoPostgreSQL/config"
 	"github.com/pglekshmi/explorerGoPostgreSQL/controllers"
 	"github.com/pglekshmi/explorerGoPostgreSQL/models"
 	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
-func BlockQuery(DB *gorm.DB){
-	Client, err := config.RetryConnectNode(5,2 * time.Second)
+func BlockQuery(DB *gorm.DB) {
+	Client, err := config.RetryConnectNode(5, 2*time.Second)
 	if err != nil {
 		fmt.Println("Error in connecting to Node")
 	}
@@ -23,126 +23,115 @@ func BlockQuery(DB *gorm.DB){
 
 	var lastBlock models.Block
 
-	if err:=DB.Last(&lastBlock).Error;err!=nil{
-			fmt.Println("Something wrong on getting data from DB")
-		}
-		
-		i := lastBlock.Number+1
-for {
-	fmt.Println("hello")
-   
-	fmt.Println("latest block",latestBlockNumber)
-    if i > latestBlockNumber {
-        break // Exit when i exceeds the latest block number
-    }
-		
-			fmt.Println("hi")
-			fmt.Println(i)
-			blocks,err:=controllers.GetBlockDetails(i)
-
-			if err!=nil{
-				fmt.Println("Cannot get block details")
-			}
-			newBlock := models.Block{
-				Number: blocks.NumberU64(),
-				Hash: blocks.Hash().Hex(),
-				ParentHash: blocks.ParentHash().Hex(),
-				GasLimit: blocks.GasLimit(),
-				Time: blocks.Time(),
-				Size: blocks.Size(),
-			}
-			if err := DB.Create(&newBlock).Error; err != nil {
-				fmt.Println("Error creating new block:", err)
-			} else {
-				fmt.Println("Block inserted successfully!")
-			}
-			 blockHash:=newBlock.Hash
-	fmt.Println("BlovkHash",blockHash)
-	fmt.Println(blocks.Body().Transactions[0].To())
-	if len(blocks.Body().Transactions)==0{
-		fmt.Println("No transactions in this block, skipping to the next block")
-    	i++  // Move to the next block
-		continue
+	if err := DB.Last(&lastBlock).Error; err != nil {
+		fmt.Println("Something wrong on getting data from DB")
 	}
-	Count := int64(0) 
-	contractCount := 0
-	contractCall := models.Contract_call{}
 
-	for _,k:= range blocks.Body().Transactions{
-		fmt.Println(k)
-		toAddress := controllers.TransAddressCheck(k)
-		tHash := controllers.TransHashCheck(k)
-		tType := controllers.TransTypeCheck(k)
-		tValue := controllers.TransValueCheck(k)
-		if k.To()==nil{
-			
-			contractCount++
-			receipt,err:= Client.TransactionReceipt(context.Background(),k.Hash())
+	i := lastBlock.Number + 1
+	for {
 
-			if err==nil{ //to store contract address to contract_call table
-				fmt.Println("Contract Address Found")
-				contractCall.Address = receipt.ContractAddress.Hex()
-				if err:=DB.Create(&contractCall).Error;err!=nil{
-					fmt.Println("Something wrong with connecting Contract call Table")
+		if i > latestBlockNumber {
+			break // Exit when i exceeds the latest block number
+		}
+
+		blocks, err := controllers.GetBlockDetails(i)
+
+		if err != nil {
+			fmt.Println("Cannot get block details")
+		}
+		newBlock := models.Block{
+			Number:     blocks.NumberU64(),
+			Hash:       blocks.Hash().Hex(),
+			ParentHash: blocks.ParentHash().Hex(),
+			GasLimit:   blocks.GasLimit(),
+			Time:       blocks.Time(),
+			Size:       blocks.Size(),
+		}
+		if err := DB.Create(&newBlock).Error; err != nil {
+			fmt.Println("Error creating new block:", err)
+		} else {
+			fmt.Println("Block inserted successfully!")
+		}
+
+		if len(blocks.Body().Transactions) == 0 {
+			fmt.Println("No transactions in this block, skipping to the next block")
+			i++ // Move to the next block
+			continue
+		}
+		Count := int64(0)
+		contractCount := 0
+		contractCall := models.Contract_call{}
+
+		for _, k := range blocks.Body().Transactions {
+
+			toAddress := controllers.TransAddressCheck(k)
+			tHash := controllers.TransHashCheck(k)
+			tType := controllers.TransTypeCheck(k)
+			tValue := controllers.TransValueCheck(k)
+			if k.To() == nil {
+
+				contractCount++
+				receipt, err := Client.TransactionReceipt(context.Background(), k.Hash())
+
+				if err == nil { //to store contract address to contract_call table
+					fmt.Println("Contract Address Found")
+					contractCall.Address = receipt.ContractAddress.Hex()
+					if err := DB.Create(&contractCall).Error; err != nil {
+						fmt.Println("Something wrong with connecting Contract call Table")
+					}
 				}
 			}
-		}
-
 
 			err := DB.Model(&models.Contract_call{}).
-					Where("Address = ?", toAddress).
-					Update("Calls", gorm.Expr("Calls + 1")).Error
+				Where("Address = ?", toAddress).
+				Update("Calls", gorm.Expr("Calls + 1")).Error
 
-					if err!= nil{
-						fmt.Println("No such contract address")
-					}
-		
-		Count++;
+			if err != nil {
+				fmt.Println("No such contract address")
+			}
 
-		newTransaction:= models.Transaction{
-			Thash:tHash,
-			// To:blocks.Body().Transactions[k].To().Hex(),
-			To:toAddress,
+			Count++
 
-			Type:tType,
-			Gas:k.Gas(),
-			Value:tValue,
-			BlockNumber:newBlock.Number,
+			newTransaction := models.Transaction{
+				Thash: tHash,
+				// To:blocks.Body().Transactions[k].To().Hex(),
+				To: toAddress,
+
+				Type:        tType,
+				Gas:         k.Gas(),
+				Value:       tValue,
+				BlockNumber: newBlock.Number,
+			}
+
+			if err := DB.Create(&newTransaction).Error; err != nil {
+				fmt.Println("Something went wrong", err)
+				log.Fatalln("Something wrong happended on transaction db")
+			}
 		}
-		fmt.Println("New Transaction",newTransaction)
-	if err:=DB.Create(&newTransaction).Error; err!=nil{
-		fmt.Println("Something went wrong",err)
-		log.Fatalln("Something wrong happended on transaction db")
-	}
-	} 
-	transCount := models.TransCount{
-		Timestamp: blocks.Time(),
-		BlockNumber: blocks.NumberU64(),
-		Count:Count,
-		ContractCount: contractCount,
-	}
-	if err := DB.Create(&transCount).Error; err != nil {
-		fmt.Println("Something went wrong", err)
-		
-	}
-	fmt.Println("All Transactions entered")
-	i++
-	fmt.Println(i)
+		transCount := models.TransCount{
+			Timestamp:     blocks.Time(),
+			BlockNumber:   blocks.NumberU64(),
+			Count:         Count,
+			ContractCount: contractCount,
 		}
+		if err := DB.Create(&transCount).Error; err != nil {
+			fmt.Println("Something went wrong", err)
+
+		}
+		fmt.Println("All Transactions entered")
+		i++
+
 	}
+}
 
-
-	func Task(DB *gorm.DB) {
-		ticker := time.NewTicker(15 * time.Second)
+func Task(DB *gorm.DB) {
+	ticker := time.NewTicker(15 * time.Second)
 	defer ticker.Stop()
 
 	for t := range ticker.C {
-	
-		
-			fmt.Println("Executing BlockQuery...",t)
-			BlockQuery(DB)
+
+		fmt.Println("Executing BlockQuery...", t)
+		BlockQuery(DB)
 
 	}
-	}
-	
-	
+}
