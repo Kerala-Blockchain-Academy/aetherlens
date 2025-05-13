e1d643fd57f4969b3d7298f89ba5f5f1fbd9eeca
pglekshmi <pglekshmi88@gmail.com>
2025-05-13 14:23:10 +0530

    🔧|Fixed

--- ui/src/views/sample-page/ContractTxns.tsx
+++ ui/src/views/sample-page/ContractTxns.tsx
@@ -47,7 +47,11 @@ const ContractTxns = () => {
     const hours = Math.floor(minutes / 60);
     if (hours < 24) return `${hours} hr${hours !== 1 ? 's' : ''} ago`;
     const days = Math.floor(hours / 24);
-    return `${days} day${days !== 1 ? 's' : ''} ago`;
+    if (days < 30) return `${days} day${days !== 1 ? 's' : ''} ago`;
+    const months = Math.floor(days / 30);
+     if (months < 12) return `${months} month${months !== 1 ? 's' : ''} ago`;
+     const years = Math.floor(months / 12);
+     return `${years} year${years !== 1 ? 's' : ''} ago`;
   };
 
   // const [contractCalls, setContractCalls] = useState([])
